b2e9c9a7924717a784305ac82e1432e06b4e41f3
김도현 <mookieum1991@gmail.com>
2022-06-16 11:15:38 +0900

    add Rest API for jwt RefreshToken and Define Route

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -8,4 +8,5 @@ module.exports = function (app) {
     });
     app.post("/api/auth/signup", [verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkRolesExisted], controller.signup);
     app.post("/api/auth/signin", controller.signin);
+    app.post("/api/auth/refreshtoken", controller.refreshToken);
 };
